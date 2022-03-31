d5be191a18f997786cc87475c8bf18f89d18b2ee
Ruslan-Gabbazov <ruslan.gabbazov01@gmail.com>
2022-03-31 20:24:34 +0300

    Task 4 Collections done

--- Task4Collections/Task4Collections/Program.cs
+++ Task4Collections/Task4Collections/Program.cs
@@ -1,3 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+var list = new List<Entity>
+{
+    new() {Id = 1, ParentId = 0, Name = "Root entity"},
+    new() {Id = 2, ParentId = 1, Name = "Child of 1 entity"},
+    new() {Id = 3, ParentId = 1, Name = "Child of 1 entity"},
+    new() {Id = 4, ParentId = 2, Name = "Child of 2 entity"},
+    new() {Id = 5, ParentId = 4, Name = "Child of 4 entity"}
+};
+
+var dictionary = MakeDictionary(list);
+
+foreach (var keys in dictionary)
+{
+    Console.Write($"Key = {keys.Key}, Value = List{{");
+    foreach (var entity in keys.Value) 
+        Console.Write($"Entity{{Id = {entity.Id}}} ");
+    
+    Console.WriteLine("}");
+}
+
+static Dictionary<int, List<Entity>> MakeDictionary(List<Entity> entitiesList)
+{
+    var newDictionary = new Dictionary<int, List<Entity>>();
+
+    foreach (var entity in entitiesList)
+    {
+        newDictionary.TryAdd(entity.ParentId, new List<Entity>());
+        newDictionary[entity.ParentId].Add(entity);
+    }
+
+    return newDictionary;
+}
+
+internal class Entity
+{
+    public int Id;
+    public int ParentId;
+    public string? Name;
+}
