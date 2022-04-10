b2f8f1b88ab8a2fe4ccb1ffea7a3e13ef091d84d
Ruslan-Gabbazov <ruslan.gabbazov01@gmail.com>
2022-04-11 01:14:13 +0300

    Task 6 done

--- Task6ParallelismAndMultithreading/Program.cs
+++ Task6ParallelismAndMultithreading/Program.cs
@@ -1,3 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Task6ParallelismAndMultithreading;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Приложение запущено");
+while (true)
+{
+    Console.WriteLine("Введите текст запроса. Для выхода введите /exit");
+    var command = Console.ReadLine();
+
+    if (command == "/exit") 
+        break;
+
+    Console.WriteLine($"Будет послано сообщение '{command}'");
+    Console.WriteLine("Введите аргументы сообщения. Если аргументы не нужны - введите /end");
+    
+    var message = command;
+    command = Console.ReadLine();
+    
+    var arguments = new List<string>();
+    while (true)
+    {
+        if (command == "/end")
+            break;
+        
+        arguments.Add(command ?? string.Empty);
+        
+        Console.WriteLine("Введите следующий аргумент сообщения. Если аргументы не нужны /end");
+        command = Console.ReadLine();
+    }
+
+    ThreadPool.QueueUserWorkItem(_ => CallHandler(message ?? string.Empty, arguments.ToArray()));
+}
+Console.WriteLine("Приложение завершает работу");
+
+
+static void CallHandler(string message, string[] arguments)
+{
+    var messageId = Guid.NewGuid().ToString("D");
+    var handler = new DummyRequestHandler();
+
+    Console.WriteLine($"Было отправлено сообщение '{message}'. " +
+                      $"Присвоен индентефикатор: {messageId}");
+    try
+    {
+        Console.WriteLine($"Сообщение с индентификатором '{messageId}' " +
+                          $"получило ответ - {handler.HandleRequest(message, arguments)}");
+    }
+    catch (Exception exception)
+    {
+        Console.WriteLine($"Сообщение с индентификатором '{messageId}' " +
+                          $"упало с ошибкой: {exception.Message}");
+    }
+}
--- Task6ParallelismAndMultithreading/Requester.cs
+++ Task6ParallelismAndMultithreading/Requester.cs
@@ -1,3 +1,5 @@
+namespace Task6ParallelismAndMultithreading;
+
 /// <summary>
 /// Предоставляет возможность обработать запрос.
 /// </summary>
